9e781f473dd41ae37a4a4dd197eb5ea75db45835
DayBreak <forregistersth@163.com>
2016-07-27 11:05:49 +0800

    添加速度控制变量

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    public float speed = 5.0f;      //添加速度控制变量，并将初始值设为5.0f
+
 	// Use this for initialization
 	void Start () {
 	
@@ -19,6 +21,6 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");         //得到垂直方向输入
         //用上面的水平方向(X轴)和垂直方向(Z轴)输入创建一个Vector3变量，作为刚体速度
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        GetComponent<Rigidbody>().velocity = movement;
+        GetComponent<Rigidbody>().velocity = movement * speed;
     }
 }
