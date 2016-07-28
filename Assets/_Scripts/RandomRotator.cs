8ae6682f4de6ebff77a6d3cff5a0335445f5c979
DayBreak <forregistersth@163.com>
2016-07-28 19:27:51 +0800

    添加小行星对象，并添加小行星随机旋转功能

---
+++ Assets/_Scripts/RandomRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+    public float tumble = 10.0f;    //小行星旋转系数
+    
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().angularVelocity /*angularVelocity为物体角速度*/ = Random.insideUnitSphere * tumble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
