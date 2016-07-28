1737776540030dedd47e2fc3fbc3faa1b92a6c05
DayBreak <forregistersth@163.com>
2016-07-28 21:26:54 +0800

    修复爆炸效果对象不消失的bug，给爆炸效果预制体添加自动销毁脚本

---
+++ Assets/_Scripts/DestroyByTime.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+
+    public float lifetime = 2.0f;   //表示游戏对象的生命周期（这里只爆炸粒子对象）
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, lifetime);  //表示lifetime秒后销毁gameObject
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
