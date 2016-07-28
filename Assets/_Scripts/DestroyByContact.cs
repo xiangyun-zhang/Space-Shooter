e87145dca7e99bb0e19b2e23459c526d6762b79c
DayBreak <forregistersth@163.com>
2016-07-28 20:17:24 +0800

    为小行星和飞船销毁时添加爆炸效果

--- Assets/_Scripts/DestroyByContact.cs
+++ Assets/_Scripts/DestroyByContact.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject explosion;        //小行星爆炸后的粒子对象
+    public GameObject playerExplosion;  //飞船碰撞后爆炸式的粒子对象
+
 	// Use this for initialization
 	void Start () {
 	
@@ -17,7 +20,12 @@ public class DestroyByContact : MonoBehaviour {
     {
         if (other.tag == "Boundary")
             return;
+
         Destroy(other.gameObject);      //销毁其他的物体
         Destroy(gameObject);            //销毁自身
+
+        Instantiate(explosion, transform.position, transform.rotation);                         //实例化explosion的位置为小行星的位置
+        if (other.tag == "Player")
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);   //playerExplos的位置为飞船位置
     }
 }
