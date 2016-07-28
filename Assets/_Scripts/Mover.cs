5b3d150b185debf6fd6e3d3fe33cccc818a9d402
DayBreak <forregistersth@163.com>
2016-07-28 11:03:19 +0800

    创建子弹

---
+++ Assets/_Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    public float speed = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
