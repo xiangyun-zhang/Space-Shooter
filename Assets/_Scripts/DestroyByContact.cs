2e4a2e1b7c8f48e36c7a90f652e806c17549d1a0
DayBreak <forregistersth@163.com>
2016-07-28 20:04:53 +0800

    添加小行星被子弹射中或是飞船撞击后会销毁的功能

---
+++ Assets/_Scripts/DestroyByContact.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Boundary")
+            return;
+        Destroy(other.gameObject);      //销毁其他的物体
+        Destroy(gameObject);            //销毁自身
+    }
+}
