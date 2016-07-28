eccfd7917db0cadab83f15b633a40b880caae4f6
DayBreak <forregistersth@163.com>
2016-07-28 21:40:01 +0800

    添加射击、爆炸、背景音效

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -35,6 +35,7 @@ public class PlayerController : MonoBehaviour {
         {
             nextFire = Time.time + fireRate;
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
         }
 
 	}
